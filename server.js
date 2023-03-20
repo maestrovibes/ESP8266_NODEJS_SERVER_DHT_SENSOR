d216b4d8774e79e093c926ad0381c1b4365182c2
unknown <kelvinmavyuva@gmail.com>
2023-03-20 13:48:36 +0300

    send data to mongoDB instead of a file

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 import express from 'express';
-import path from 'path';
-import fs from 'fs';
+import mongoose from 'mongoose';
+import router from './routes/router.js';
 
 const app = express();
-
-// constants
-const DB_PATH = path.resolve("db.json");
-const router = express.Router();
 
 //const PORT = process.env.PORT || 8000;
 const PORT = 6000;
@@ -14,47 +10,9 @@ const PORT = 6000;
 app.use(express.json());
 app.use("/api", router);
 
-// routes
-router.get("/greet", async (req, res, next) => {
-    res.send("Hello KASEE!!")
-});
-
-let dbFilePath = path.resolve('./db.json');
-
-// If the db.json file doesn't exist, create it
-if (!fs.existsSync(dbFilePath)) {
-    let db = {
-        data: []
-    };
-
-    fs.writeFileSync(dbFilePath, JSON.stringify(db));
-}
-
-router.post("/", async (req, res, next) => {
-    let db = JSON.parse(fs.readFileSync(dbFilePath));
-
-    //let temperature = Math.floor(Math.random() * 100);
-    //let humidity = Math.floor(Math.random() * 100);
-    let body = req.body;
-    let temperature = body.temperature;
-    let humidity = body.humidity;
-
-    db.data.push({
-        temperature: temperature,
-        humidity: humidity
-    });
-
-    fs.writeFileSync(dbFilePath, JSON.stringify(db));
-
-    res.status(200).send('Data posted successfully!');
-});
-
-router.get("/", async (req, res, next) => {
-    let db = JSON.parse(fs.readFileSync(dbFilePath));
-    let lastValue = db.data[db.data.length - 1];
-
-    res.status(200).send(lastValue);
-});
-
-
-app.listen(PORT, () => console.log("Listening on port", PORT));
+mongoose.connect("mongodb+srv://esp1234:esp1234@cluster0.afpexgq.mongodb.net/?retryWrites=true&w=majority")
+.then(()=> app.listen(PORT))
+.then(()=> 
+    console.log("Connected to database and listening on port", PORT)
+)
+.catch((err)=> console.log(err));
